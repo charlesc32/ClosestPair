af6a0a0d54a17daec302dd5888ec3cb1ecbe7495
ccymbor <ccymbor@gmail.com>
2013-08-09 21:35:36 -0700

    init

---
+++ ClosestPair/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClosestPairFinder.Process(args[0]);
+    }
+}
+
+public static class ClosestPairFinder
+{
+    public static void Process(string fileName)
+    {
+        var lines = new List<string>();
+        using (StreamReader reader = File.OpenText(fileName))
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                if (null == line)
+                    continue;
+                lines.Add(line);
+            }
+
+        //Each pair of coordinates is a Tuple of int, int
+        //Each set of coordinates to be tested is a List of the Tuples
+        //Finally all sets are put in to a List of their own
+        List<List<Tuple<int, int>>> inputs = ParseInput(lines);
+
+        var output = Calculate(inputs);
+        foreach (var item in output)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    private static ParallelQuery<string> Calculate(List<List<Tuple<int, int>>> input)
+    {
+        var output = input.AsParallel().AsOrdered().Select(set => FindClosestPairInSet(set));
+        return output;
+    }
+
+    private static List<List<Tuple<int, int>>> ParseInput(List<string> lines)
+    {
+        var parsedLines = new List<List<Tuple<int, int>>>();
+        int x = 0;
+        int y = 0;
+        string[] coordinates;
+        var currentSet = new List<Tuple<int, int>>();
+
+        for (int i = 1; i < lines.Count; i++)
+        {
+            if (lines[i].Length == 1)
+            {
+                parsedLines.Add(currentSet.ToList());
+                currentSet.Clear();
+                continue;
+            }
+            else
+            {
+                coordinates = lines[i].Split(' ');
+            
+                x = int.Parse(coordinates[0]);
+                y = int.Parse(coordinates[1]);
+
+                currentSet.Add(new Tuple<int, int>(x, y));
+            }
+        }
+
+        return parsedLines;
+    }
+
+    private static string FindClosestPairInSet(List<Tuple<int, int>> set)
+    {
+        double closestSoFar = double.MaxValue;
+
+        for (int i = 0; i < set.Count; i++)
+        {
+            for (int x = 0; x < set.Count; x++)
+            {
+                if (i == x) continue;
+
+                double calculatedDistance = CalculateDistance(set[i], set[x]);
+                if (calculatedDistance < closestSoFar) closestSoFar = calculatedDistance;
+            }
+        }
+
+        string ret = (closestSoFar >= 10000) ? "INFINITY" : Math.Round(closestSoFar, 4).ToString("0.0000");
+        return ret;
+    }
+
+    private static double CalculateDistance(Tuple<int, int> point1, Tuple<int, int> point2)
+    {
+        double a = point1.Item1 - point2.Item1;
+        double b = point1.Item2 - point2.Item2;
+        return Math.Sqrt((a * a) + (b * b));
+    }
+}
---
+++ ClosestPairTests/ClosestPairTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+//using ClosestPair;
+using System.Linq;
+
+namespace ClosestPairTests
+{
+    [TestClass]
+    public class ClosestPairsTests
+    {
+        private List<List<Tuple<int, int>>> exampleInput;
+        [TestInitialize()]
+        public void Initialize()
+        {
+            exampleInput = new List<List<Tuple<int, int>>>() {
+                new List<Tuple<int, int>>() {
+                    new Tuple<int, int>(0, 0), 
+                    new Tuple<int, int>(10000, 10000), 
+                    new Tuple<int, int>(20000, 20000)
+                },
+                new List<Tuple<int, int>>() {
+                    new Tuple<int, int>(0, 2),
+                    new Tuple<int, int>(6, 67),
+                    new Tuple<int, int>(43, 71),
+                    new Tuple<int, int>(39, 107),
+                    new Tuple<int, int>(189, 140)
+                }
+            };
+        }
+        
+        [TestMethod]
+        public void TestParseInput()
+        {
+            var lines = new List<string>();
+            lines.Add("3");
+            lines.Add("0 0");
+            lines.Add("10000 10000");
+            lines.Add("20000 20000");
+            lines.Add("5");
+            lines.Add("0 2");
+            lines.Add("6 67");
+            lines.Add("43 71");
+            lines.Add("39 107");
+            lines.Add("189 140");
+            lines.Add("0");
+
+            var expected = exampleInput;
+             
+            PrivateType cpf = new PrivateType(typeof(ClosestPairFinder));
+            var actual = (List<List<Tuple<int, int>>>)cpf.InvokeStatic("ParseInput", lines);
+            CollectionAssert.AreEqual(expected[0], actual[0]);
+        }
+
+        [TestMethod]
+        public void TestFindClosestPair()
+        {
+            PrivateType cpf = new PrivateType(typeof(ClosestPairFinder));
+            var actual = cpf.InvokeStatic("FindClosestPairInSet", exampleInput[1]);
+            Assert.AreEqual(actual, "36.2215");
+        }
+
+        [TestMethod]
+        public void TestCalculate()
+        {
+            PrivateType cpf = new PrivateType(typeof(ClosestPairFinder));
+            var example = new List<List<Tuple<int, int>>>();
+            example.Add(exampleInput[1]);
+            example.Add(exampleInput[1]);
+            var actual = (ParallelQuery<string>)cpf.InvokeStatic("Calculate", example);
+            
+            foreach (var item in actual)
+            {
+                Assert.AreEqual(item, "36.2215");    
+            }
+            
+        }
+    }
+}
